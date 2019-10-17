e37be58eb907f9725ac09d59744d6459abdcbfc1
bpawanchand <bpawanchand@gmail.com>
2019-10-17 08:35:23 +0200

    from sar laptop

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Main = require('./main');
+
+ReactDOM.render(<Main />,document.getElementById('app'));
---
+++ src/main.js
@@ -0,0 +1,6 @@
+const React = require('react');
+const Main = () => {
+    return( <h1> This is from App Main</h1>);
+}
+
+module.exports = Main;
